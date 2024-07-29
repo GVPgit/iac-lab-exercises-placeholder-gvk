865b8b9786cbc45689cf60b31ab97462668dee01
guru.vk@thoughtworks.com <guru.vk@users.noreply.github.com>
2024-07-29 19:40:57 +0530

    thirdweekompot

---
+++ third_week_import/import.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-2"
+}
+resource "aws_vpc" "main" {
+  cidr_block           = "192.168.1.0/25"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "main"
+  }
+}
